611273d7153a6956850fd59de56317e3fa142718
Mike Dragert <mikedragert@gmail.com>
2024-01-17 13:17:54 -0500

    implemented wasd

--- input.js
+++ input.js
@@ -1,20 +1,36 @@
-const setupInput = function() {
+let connection = undefined;
+
+const setupInput = function(_connection) {
+  connection = _connection;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", handleUserInput)
+  stdin.on("data", handleUserInput);
   return stdin;
-}
+};
 
 const handleUserInput = function(data) {
-  switch(data) {
-    case '\u0003':
-      process.exit();
-    default:
-      console.log(data);
-      break;
+  switch (data) {
+  case '\u0003':
+    process.exit();
+    break;
+  case 'w':
+    connection.write("Move: up");
+    break;
+  case 'a':
+    connection.write("Move: left");
+    break;
+  case 's':
+    connection.write("Move: down");
+    break;
+  case 'd':
+    connection.write("Move: right");
+    break;
+  default:
+    console.log(data);
+    break;
   }
-}
+};
 
 module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,5 +1,5 @@
 const connect = require("./client.js");
-const setupInput= require("./input.js");
+const setupInput = require("./input.js");
 
-connect();
-setupInput();
+let connection = connect();
+setupInput(connection);
