699404d17ecef67f61bd5e8d7364919cfb825867
Mike Dragert <mikedragert@gmail.com>
2024-01-17 12:55:02 -0500

    watching keyboard input and allow exit on ctrl-c

--- play.js
+++ play.js
@@ -1,7 +1,18 @@
 const {connect} = require("./client.js");
 
+
+
 console.log("Connecting ...");
 const conn = connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+}
+
 
 conn.on("connect", () => {
   console.log("Successfully connected to game server");
@@ -11,6 +22,21 @@ conn.on("connect", () => {
 conn.on("data", (data) => {
   console.log(data);
 });
+
+const handleUserInput = function(data) {
+  switch(data) {
+    case '\u0003':
+      process.exit();
+    default:
+      console.log(data);
+      break;
+  }
+}
+
+const input = setupInput();
+
+input.on("data", handleUserInput)
+
 
 // setInterval(() => {
 //   conn.write("Move: up");
